24bd13535cd1eda740da3c5b2fd1123d87ce575d
Redwan Albadawi <redwansy1976@gmail.com>
2026-08-12 21:59:24 -0400

    Build initial storefront foundation

---
+++ src/app/product-card/product-card.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../product';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [],
+  templateUrl: './product-card.html',
+  styleUrl: './product-card.css',
+})
+export class ProductCard {
+  @Input()
+  product!: Product;
+}
